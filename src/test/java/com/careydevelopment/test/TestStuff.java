906b059677801558e748b5236b04610be586665a
brianmcarey <careyb@mindspring.com>
2016-03-30 19:37:06 -0400

    added test

---
+++ src/test/java/com/careydevelopment/test/TestStuff.java
@@ -0,0 +1,12 @@
+package com.careydevelopment.test;
+
+import junit.framework.TestCase;
+
+public class TestStuff extends TestCase {
+
+	public void testTheThing() {
+		String dude = "dude";
+		assertTrue(dude.equals("dude"));
+	}
+	
+}
